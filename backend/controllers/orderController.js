a72d689e4bf7b8907defe5cccfb0e5081f837c80
mega-bytes111 <arpantiwari381@gmail.in>
2026-07-20 21:15:54 +0530

    file name issue

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 const Order = require("../models/Order");
-const User = require("../models/userModel");
+const User = require("../models/User");
 const sendEmail = require("../utils/sendEmail");
 const orderPlacedTemplate = require("../utils/emailTemplates/orderPlacedTemplate");
 const orderDeliveredTemplate = require("../utils/emailTemplates/orderDeliveredTemplate");
